5d7d0ea311c288073680caba56ed06a6a1e5d392
Naina <naina@example.com>
2026-05-19 14:20:53 +0530

    changed files

--- superchat-llm/backend/routes/chatRoutes.js
+++ superchat-llm/backend/routes/chatRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const message = require("../models/message");
 
 const router = express.Router();
 const upload = multer(); // Middleware to parse multipart/form-data (FormData)
@@ -18,9 +19,16 @@ router.post("/", upload.none(), async (req, res) => {
     // Optional: Use the language preference from the frontend to guide the AI
     const prompt = language ? `[System: Respond in ${language}] ${message}` : message;
 
+
     const result = await model.generateContent(prompt);
 
     const reply = result.response.text();
+    const savedmessage = await message.create({
+      userMessage: message,
+      aiResponse: reply,
+
+    })
+    console.log("Saved:", savedmessage);
 
     res.json({ reply });
   } catch (error) {
